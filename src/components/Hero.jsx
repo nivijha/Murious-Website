6e131537954c7a567449a6116fa1d5911963a848
Manik2375 <manikkpr2375@gmail.com>
2025-01-31 21:54:30 +0530

    fix: remove unnecessary test class from Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -48,7 +48,7 @@ const Hero = () => {
             Register Now
           </Button>
         </div>
-        <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24 test">
+        <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
           <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
             <div className="relative bg-n-8 rounded-[1rem]">
               <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
