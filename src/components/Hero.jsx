22763e84753680b898e7a89eaa9ee7e11d673e33
Manik2375 <manikkpr2375@gmail.com>
2025-01-31 18:38:55 +0530

    fix: improve the background image of hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,10 +21,10 @@ const Hero = () => {
       id="hero"
     >
       <div className="container relative" ref={parallaxRef}>
-      <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
+        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
             Explore the Possibilities and dive into {` `}
-             {` `}
+            {` `}
             <span className="inline-block relative">
               Murious 19.0{" "}
               <img
@@ -37,25 +37,32 @@ const Hero = () => {
             </span>
           </h1>
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
-          Bringing You a Realm of Endless Excitement. Dare to Explore the Extraordinary.
+            Bringing You a Realm of Endless Excitement. Dare to Explore the
+            Extraordinary.
           </p>
-          <Button href="https://docs.google.com/forms/d/e/1FAIpQLSeMYbCu5XAxFIFfsS6RqPkeUnaoe2H_SiDHCxWaYvktGGiyTg/viewform" className="text-xl"white>
+          <Button
+            href="https://docs.google.com/forms/d/e/1FAIpQLSeMYbCu5XAxFIFfsS6RqPkeUnaoe2H_SiDHCxWaYvktGGiyTg/viewform"
+            className="text-xl"
+            white
+          >
             Register Now
           </Button>
         </div>
-        <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
+        <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24 test">
           <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
             <div className="relative bg-n-8 rounded-[1rem]">
               <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
 
               <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
-                <a href="https://drive.google.com/file/d/1ITFYqmm-qMQJMhyN3TYLCSlIDyrgyH3t/view?usp=sharing"><img
-                  src={murious}
-                  className="w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]"
-                  width={1024}
-                  height={490}
-                  alt="AI"
-                /></a>
+                <a href="https://drive.google.com/file/d/1ITFYqmm-qMQJMhyN3TYLCSlIDyrgyH3t/view?usp=sharing">
+                  <img
+                    src={murious}
+                    className="w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]"
+                    width={1024}
+                    height={490}
+                    alt="AI"
+                  />
+                </a>
 
                 {/* <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2" /> */}
 
@@ -80,7 +87,7 @@ const Hero = () => {
 
             <Gradient />
           </div>
-          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
+          <div className="absolute -top-[54%] left-1/2 w-[234%] scale-[2.5] md:scale-100 -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
             <img
               src={heroBackground}
               className="w-full"
